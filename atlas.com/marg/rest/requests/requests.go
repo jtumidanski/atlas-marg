4cb9fce5c725a03a9ddee63e1970614e3b6252a7
Tumidanski Justin D <Justin.Tumidanski@dematic.com>
2021-08-12 10:24:23 -0400

    Adjust REST GET function to be fault tolerant (if desired). Migrate requests to use that common function.

--- atlas.com/marg/rest/requests/requests.go
+++ atlas.com/marg/rest/requests/requests.go
@@ -1,5 +1,60 @@
 package requests
 
+import (
+	"atlas-marg/json"
+	"atlas-marg/retry"
+	"github.com/sirupsen/logrus"
+	"net/http"
+)
+
 const (
 	BaseRequest string = "http://atlas-nginx:80"
 )
+
+type configuration struct {
+	retries int
+}
+
+type Configurator func(c *configuration)
+
+func SetRetries(amount int) Configurator {
+	return func(c *configuration) {
+		c.retries = amount
+	}
+}
+
+func Get(l logrus.FieldLogger) func(url string, resp interface{}, configurators ...Configurator) error {
+	return func(url string, resp interface{}, configurators ...Configurator) error {
+		c := &configuration{retries: 1}
+		for _, configurator := range configurators {
+			configurator(c)
+		}
+
+		var r *http.Response
+		get := func(attempt int) (bool, error) {
+			var err error
+			r, err = http.Get(url)
+			if err != nil {
+				l.Warnf("Failed calling GET on %s, will retry.", url)
+				return true, err
+			}
+			return false, nil
+		}
+		err := retry.Try(get, c.retries)
+		if err != nil {
+			l.WithError(err).Errorf("Unable to successfully call GET on %s.", url)
+			return err
+		}
+		err = processResponse(r, resp)
+		return err
+	}
+}
+
+func processResponse(r *http.Response, rb interface{}) error {
+	err := json.FromJSON(rb, r.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
